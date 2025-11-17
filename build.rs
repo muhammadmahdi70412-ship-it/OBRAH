e9c13fd27374534a8735bd1002ea8bb78b136a99
The Lord of Apples <muhammadmahdi70412@gmail.com>
2025-11-17 14:23:59 +0400

    Changed build.rs slightly

--- build.rs
+++ build.rs
@@ -1,9 +1,13 @@
 fn main() {
-    // Try to read the environment variable OPENCL_SDK.
-    if let Ok(path) = std::env::var("OPENCL_SDK") {
-        println!("cargo:rustc-link-search=native={}/lib", path);
-        println!("cargo:rustc-link-lib=dylib=OpenCL");
+    if cfg!(target_os = "linux") {
+        println!("cargo:rustc-link-lib=OpenCL");
     } else {
-        println!("cargo:warning=Environment variable OPENCL_SDK not set. Please set it to your OpenCL SDK path.");
+        // Try to read the environment variable OPENCL_SDK.
+        if let Ok(path) = std::env::var("OPENCL_SDK") {
+            println!("cargo:rustc-link-search=native={}/lib", path);
+            println!("cargo:rustc-link-lib=dylib=OpenCL");
+        } else {
+            println!("cargo:warning=Environment variable OPENCL_SDK not set. Please set it to your OpenCL SDK path.");
+        }
     }
 }
