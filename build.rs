c34d90e6d14ca883974d89b0c447c463cb8a6484
The Lord of Apples <muhammadmahdi70412@gmail.com>
2025-11-17 20:32:52 +0400

    Made linking with OpenCL a little better.

--- build.rs
+++ build.rs
@@ -1,13 +1,16 @@
+use std::path::Path;
+
 fn main() {
-    if cfg!(target_os = "linux") {
-        println!("cargo:rustc-link-lib=OpenCL");
+    let clpath = Path::new("C:\\Windows\\System32\\OpenCL.dll");
+    if clpath.exists() {
+       	println!("cargo:rustc-link-lib=OpenCL");
     } else {
         // Try to read the environment variable OPENCL_SDK.
         if let Ok(path) = std::env::var("OPENCL_SDK") {
-            println!("cargo:rustc-link-search=native={}/lib", path);
+      	    println!("cargo:rustc-link-search=native={}/lib", path);
             println!("cargo:rustc-link-lib=dylib=OpenCL");
         } else {
-            println!("cargo:warning=Environment variable OPENCL_SDK not set. Please set it to your OpenCL SDK path.");
+            println!("cargo:error=Environment variable OPENCL_SDK not set. Set it to your OpenCL SDK path.");
         }
     }
 }
